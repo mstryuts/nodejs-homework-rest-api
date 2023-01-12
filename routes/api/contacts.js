f10ca0e5b13fc503bbd71c478a81475a6bb0c41c
maksim stryuts <maxim.stryuts@gmail.com>
2023-01-12 16:34:13 +0200

    11

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  res.json({ message: "template message1" });
+  res.json({ message: "template message11" });
 });
 
 router.get("/:contactId", async (req, res, next) => {
